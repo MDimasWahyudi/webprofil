021e0dcfb6f90a0cf3d58c7b17a581e1f450b0a4
MDimasWahyudi <wahdimas57@gmail.com>
2026-07-26 22:16:56 +0700

    belajar buat web profil

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil Website</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<header>
+    <h1>Website Portofolio</h1>
+
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Tentang</a>
+        <a href="#">Skill</a>
+        <a href="#">Portofolio</a>
+        <a href="#">Kontak</a>
+    </nav>
+</header>
+
+<section class="hero">
+    <h2> Halo nama saya Dimas</h2>
+    <h2> saya mahasiswa Sistem Informasi Gundarma</h2>
+    <h2> Sedang mencoba menjadi Web Developer</h2>
+    <p> Ini pengalaman pertama saya belajar PHP sendiri</p>    
+    <button> Tentang saya </button>
+</section>
+
+<footer>
+    @Dimas 2026
+</footer>
+</body>
+</html>
